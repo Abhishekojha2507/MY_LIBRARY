e3ce6a2afe30b58ed45ed0f79899c991be338dc9
Vivekanand ojha <goluojha13101992@gmail.com>
2023-01-30 00:17:49 +0530

    Commiting Mini-Library Project

--- indexes6.js
+++ indexes6.js
@@ -1,29 +1,30 @@
 console.log("this is Es6 version of project");
-if(localStorage.getItem('t')==null)
-{localStorage.setItem('t',0)};
+if (localStorage.getItem("t") == null) {
+  localStorage.setItem("t", 0);
+}
 
-//  step4 if page get refreshed content sholud not be vanished 
+//  step4 if page get refreshed content sholud not be vanished
 // function refresh ()
 // {
 //     let storedBook=JSON.parse(localStorage.getItem('allData'))
-//     let uiString=""; 
+//     let uiString="";
 //     console.log(storedBook)
 //     if(storedBook!=null)//will not be null only lenght reduces.
 //     {if(storedBook.length!=0)//hence this becomes important
 //    { storedBook.forEach(function(book,i){
 //         let tablebody = document.getElementById("tablebody");
-//         uiString += `<tr id=${book.id}> 
+//         uiString += `<tr id=${book.id}>
 //         <th scope="row">${i+1}</th>
 //         <td>${book.name}</td>
 //         <td>${book.author}</td>
 //         <td >${book.type}</td>
 //         <td><button class="button" onclick="del(event)" id=${i}>Delete</button></td>
 //         </tr>`;
-        
+
 //         tablebody.innerHTML = uiString;
 //         if(i>3)
 //     {
-      
+
 //        divTable.style.border='5px solid';
 //        divTable.style.overflowY='scroll';
 //        divTable.style.overflowX='hidden';
@@ -36,7 +37,7 @@ if(localStorage.getItem('t')==null)
 //        divTable.style.overflowY='';
 //        divTable.style.overflowX='';
 //        thead.style.position=''
-//        thead.style.top='' 
+//        thead.style.top=''
 
 //     }
 //       })
@@ -48,108 +49,105 @@ if(localStorage.getItem('t')==null)
 //     }
 //     }
 
-    
 // step2 constructor to create object book
-class Book{
-    constructor(name,author,type)
-    {
-        this.name=name;
-        this.author=author;
-        this.type=type;
-    }
+class Book {
+  constructor(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+  }
 }
 let tablebody = document.getElementById("tablebody");
 // step3 creating methods to br inherited for using in book object
-class Display
-{
-    add() {
-      let storedBook=JSON.parse(localStorage.getItem('allData'))
-      let uiString=""; 
-      // console.log(storedBook)
-      if(storedBook!=null)//will not be null only lenght reduces.
-      {if(storedBook.length!=0)//hence this becomes important
-     { storedBook.forEach(function(book,i){
+class Display {
+  add=function() {
+    let storedBook = JSON.parse(localStorage.getItem("allData"));
+    let uiString = "";
+    // console.log(storedBook)
+    if (storedBook != null) {
+      //will not be null only lenght reduces.
+      if (storedBook.length != 0) {
+        //hence this becomes important
+        storedBook.forEach(function (book, i) {
           let tablebody = document.getElementById("tablebody");
           uiString += `<tr id=${book.id}> 
-          <th scope="row">${i+1}</th>
+          <th scope="row">${i + 1}</th>
           <td>${book.name}</td>
           <td>${book.author}</td>
           <td>${book.type}</td>
           <td><button class="button" onclick="del(event)" id=${i}>Delete</button></td>
           </tr>`;
-          
-          tablebody.innerHTML = uiString;
-          if(i>3)
-      {
-        
-         divTable.style.border='5px solid';
-         divTable.style.overflowY='scroll';
-         divTable.style.overflowX='hidden';
-         thead.style.position='sticky'
-         thead.style.top='-1px'
-      }
-      else
-      {
-        divTable.style.border='';
-         divTable.style.overflowY='';
-         divTable.style.overflowX='';
-         thead.style.position=''
-         thead.style.top='' 
-  
-      }
-        })
-      }
-        else
-  
-        { console.log('NO element in localStorage')
-          document.getElementById("tablebody").innerHTML="";}
-      }
-      }
-  
- 
- 
-    clear (){
-        libraryForm.reset();
-         libBookName.focus();
-      };
 
-      validate (book) {
-        if (book.name.length < 2 || book.author.length < 2) 
-        return false;
-        else 
-        return true;
-      };
-    // message for successfull submision else error
-      show = function (type,displayMessage) {
+          tablebody.innerHTML = uiString;
+          if (i > 3) {
+            divTable.style.border = "5px solid";
+            divTable.style.overflowY = "scroll";
+            divTable.style.overflowX = "hidden";
+            thead.style.position = "sticky";
+            thead.style.top = "-1px";
+          } else {
+            divTable.style.border = "";
+            divTable.style.overflowY = "";
+            divTable.style.overflowX = "";
+            thead.style.position = "";
+            thead.style.top = "";
+          }
+        });
+      } else {
+        console.log("NO element in localStorage");
+        document.getElementById("tablebody").innerHTML = "";
+      }
+    } else {
+      console.log("localStorage has empty data of 'alldata'");
+    }
+  }
+
+  clear() {
+    libraryForm.reset();
+    libBookName.focus();
+  }
+
+  validate(book) {
+    if (book.name.length < 2 || book.author.length < 2) return false;
+    else return true;
+  }
+  // message for successfull submision else error
+  show = function (type, displayMessage) {
     let boldText;
-        let message = document.getElementById("message");
-        if(type==='success'){boldText='Success!'}
-        else{ boldText='Error!'}
-        message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
-      <strong>${boldText}</strong> ${displayMessage}
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>`;
-      setTimeout(function() {
-          message.innerHTML="";
-      }, 6000);
-      };
+    let message = document.getElementById("message");
+    if (type === "success") {
+      boldText = "Success!";
+    } else {
+      boldText = "Error!";
+    }
+    message.style.display="block"
+    message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+    <strong>${boldText}</strong> ${displayMessage}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`;
+    setTimeout(function () {
+      message.style.display="none"
+      // message.innerHTML = "";
+    }, 2000);
+  };
 }
 
-
-
 // step1 (create book content)
-let libraryForm=document.getElementById('libraryForm')
-let libBookName=document.getElementById('BookName')
-let id='table';
+let libraryForm = document.getElementById("libraryForm");
+let libBookName = document.getElementById("BookName");
+let id = "table";
 libraryForm.addEventListener("submit", libraryFormSubmit);
 function libraryFormSubmit(e) {
   console.log(this); //returns form object
   let name = document.getElementById("BookName").value;
   let author = document.getElementById("Author").value;
   let Fiction = document.getElementById("Fiction");
+  // can do this operation --> Fiction.checked=false
+  // false
   let Programming = document.getElementById("Programming");
   let Sports = document.getElementById("Sports");
   let type;
+  let book = new Book(name, author, type);
   if (Fiction.checked) {
     type = Fiction.value;
   } else if (Programming.checked) {
@@ -158,67 +156,52 @@ function libraryFormSubmit(e) {
     type = Sports.value;
   }
   e.preventDefault();
-  let book = new Book(name, author, type);
-  console.log(this); 
+  // console.log(this);
 
-// storing data to localStorage for refreshed page
-  let dataRetrieved=JSON.parse(localStorage.getItem('allData'))
-  if(dataRetrieved==null)
-  {
-     allData=[]
+  // storing data to localStorage for refreshed page
+  let dataRetrieved = JSON.parse(localStorage.getItem("allData"));
+  if (dataRetrieved == null) {
+    allData = [];
     //  localStorage.setItem('allData',JSON.stringify(allData))
+  } else {
+    allData = JSON.parse(localStorage.getItem("allData"));
   }
-  else
-  {
-     allData=JSON.parse(localStorage.getItem('allData'));
-  }
- //   validation
-// using Display constructor for object's prototype
-let display = new Display();
-if (display.validate(book)) {
-    let t=localStorage.getItem('t')
+  
+  // validation
+  // using Display constructor for object's prototype
+  let display = new Display();
+  if (display.validate(book)) {
+    let t = localStorage.getItem("t");
     t++;
-    localStorage.setItem('t',t)
-    id=id+t;
-    book.id=id;
-    allData.push(book);//then adding after being data gets varified
-    display.clear(book);
-    localStorage.setItem('allData',JSON.stringify(allData))
+    localStorage.setItem("t", t);
+    id = id + t;
+    book.id = id;
+    allData.push(book); //then adding after being data gets varified
+    display.clear();
+    localStorage.setItem("allData", JSON.stringify(allData));
     display.add();
-    display.show('success','your book has been successfully added')
+    display.show("success", "your book has been successfully added");
     console.log(book);
   } else {
-    display.show("danger",'Sorry you cannot add this book');
-   
+    display.show("danger", "Sorry you cannot add this book (lenght of Name or Author should be more than one character)");
   }
 }
 
-let refresh=new Display()
-refresh.add()
+let refresh = new Display();
+refresh.add();
 //step5 Selective Deletion
-function del(i)
-{
-   s=i.target.id;
-   refinedData=JSON.parse(localStorage.getItem('allData'));
-   refinedData.splice(s,1);//this will just reduce the length of array,hence will not be null
-   localStorage.setItem('allData',JSON.stringify(refinedData)); 
-   refresh.add();
-   console.log(i); //returnd window object global
+function del(i) {
+  s = i.target.id;
+  refinedData = JSON.parse(localStorage.getItem("allData"));
+  refinedData.splice(s, 1); //this will just reduce the length of array,hence will not be null
+  localStorage.setItem("allData", JSON.stringify(refinedData));
+  refresh.add();
+  console.log(i); //returnd window object global
 }
-
-
-
-
-
-
-
-
-
-
 
 // experiments
 // class chl{
-    //   constructor(name,exp)
+//   constructor(name,exp)
 //   {
 //     this.name=name;
 //     this.exp=exp;
@@ -230,7 +213,7 @@ function del(i)
 //     }
 // }
 // newVar=new chl('vivek',10)
-// console.log(newVar.dd('vivekanand',23))
+// console.log(newVar)
 
 // a=['assd',"qwq",34,43]
 // function d(x,y){
